b4dd63beeb3ead70f83086b2eca8ce02882c0078
Eric Scott <eric.scott@acm.org>
2021-12-19 07:58:17 -0800

    v. 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject ont-app/datascript-graph "0.1.1-SNAPSHOT"
+(defproject ont-app/datascript-graph "0.1.1"
   :description "Defines a datascript implementation of the IGraph protocol."
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
