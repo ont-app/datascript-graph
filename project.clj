3f029ee42fec820c58b396c3482916eb61baa381
Eric Scott <eric.scott@acm.org>
2021-12-19 07:44:39 -0800

    Dependencies reform

--- project.clj
+++ project.clj
@@ -3,14 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/clojurescript "1.10.520"]
-                 [org.clojure/spec.alpha "0.2.176"]
+  :dependencies [[ont-app/graph-log "0.1.5"]
                  ;; 3rd party libs:
-                 [datascript "0.18.10"]
-                 ;; ont-app libs:
-                 [ont-app/graph-log "0.1.1-SNAPSHOT"]
-                 [ont-app/igraph "0.1.5-SNAPSHOT"]
+                 [datascript "1.3.2"]
                  ]
   :plugins [[lein-cljsbuild "1.1.7"
              :exclusions [[org.clojure/clojure]]]
@@ -40,7 +35,13 @@
    } ;; cljsbuild
 
 
-  :profiles {:uberjar {}}
+  :profiles {:uberjar {}
+             :dev {:dependencies
+                   [[org.clojure/clojure "1.10.3"]
+                    [org.clojure/clojurescript "1.10.896"]
+                    [lein-doo "0.1.11"]
+                    ]}
+             }
   :codox {:output-path "doc"}
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                     "resources/test"
